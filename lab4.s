18c0e70fd32ae85e7ee3f9981e0a51f2badb86bd
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-02-29 13:52:23 -0500

    GOT RED WORKING

--- lab4.s
+++ lab4.s
@@ -6,6 +6,7 @@ lab4:
 
           ; Your code is placed here
     BL read_tiva_pushbutton
+    BL illuminate_RGB_LED
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	MOV pc, lr
@@ -17,10 +18,16 @@ illuminate_RGB_LED:
 
           ; Your code is placed here
     ;Initialize Clock Address
+    ; Color is passed in from r0
+    ; r0 = 1 -> RED
+    ; r0 = 2 -> Blue
+    ; r0 = 3 -> Green
+    MOV r0, #1
+
     MOV r1, #0xE000
     MOVT r1, #0x400F
     LDRB r5, [r1, #0x608] 
-    ORR r5, r5, #0x10	; Find Port F 
+    ORR r5, r5, #0x20	; Find Port F 0010 0000
     STRB r5, [r1, #0x608] ; Enable clock for Port F
 
     ; Initialize r3 with Port F address
@@ -29,31 +36,40 @@ illuminate_RGB_LED:
     MOVT r3, #0x4002
     
     LDRB r6, [r3, #0x400] ;offset 0x400 to port F
-    LDRB r7, [r3, #0x400]
-    LDRB r8, [r3, #0x400]
-    
-    ORR r6, r6, #0x02 ; configure pin 1 as output, red
-    ORR r7, r7, #0x04 ; configure pin 2 as output , blue
-    ORR r8, r8, #0x08 ; configure pin 3 as output , green
-    
+    ORR r6, r6, #0x0E ; configure pin 1 as output, red, 0000 1110 thats 0E
     STRB r6, [r3, #0x400] ; write 1 to mem
-    STRB r7, [r3, #0x400] ; write 1 to mem
-    STRB r8, [r3, #0x400] ; write 1 to mem
 
     ; Intilize pins as digital
-    LDRB r6, [r3, #0x51C]
     LDRB r7, [r3, #0x51C]
-    LDRB r8, [r3, #0x51C]
-
-    ORR r6, r6, #0x02 ; enable pin 1 as digital
-    ORR r7, r7, #0x04 ; enable pin 2 as digital
-    ORR r8, r8, #0x08 ; enable pin 3 as digital
-
-    STRB r6, [r3, #0x51C] ;write 1 to make pin digital
+	; enable pin 1, 2, 3 as digital 0000 1110
+    ORR r7, r7, #0x0E ;
     STRB r7, [r3, #0x51C] ;write 1 to make pin digital
-    STRB r8, [r3, #0x51C] ;write 1 to make pin digital
 
-    ;how to control what colors show up ?? 
+	CMP r0, #1
+	BEQ RED
+
+    ;how to control what colors show up ??
+
+RED:
+    ; RED , turn on pin 1, turn off pin 2 and 3
+    LDRB r8, [r3, #0x3FC] ; GPIODATA
+
+    ORR r9, r9, #0x02     ; need to turn pin 1 on that is 0000 0010
+
+    STRB r9, [r3, #0x3FC]		; Turn on 0000 0010
+	B COLOR_STOP
+
+    ; BLUE
+
+    ; GREEN
+
+    ; PURPLE
+
+    ; YELLOW
+
+    ; WHITE
+
+COLOR_STOP:
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	MOV pc, lr

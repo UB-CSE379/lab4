aff122d394d380139ad7b3bfc802dc1f47f69dc0
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-02-29 14:46:56 -0500

    NEED TO TEST RGB LED PART 1

--- lab4.s
+++ lab4.s
@@ -19,10 +19,12 @@ illuminate_RGB_LED:
           ; Your code is placed here
     ;Initialize Clock Address
     ; Color is passed in from r0
-    ; r0 = 1 -> RED
-    ; r0 = 2 -> Blue
-    ; r0 = 3 -> Green
-    MOV r0, #1
+    MOV r0, #1 	;RED 
+    ;MOV r0, #2 ;BLUE
+    ;MOV r0, #3 ;GREEN
+    ;MOV r0, #4 ;PURPLE -> RED AND BLUE
+    ;MOV r0, #5 ;YELLOW -> RED AND GREEN
+    ;MOV r0, #6 ;WHITE -> RED, BLUE, AND GREEN
 
     MOV r1, #0xE000
     MOVT r1, #0x400F
@@ -44,30 +46,63 @@ illuminate_RGB_LED:
 	; enable pin 1, 2, 3 as digital 0000 1110
     ORR r7, r7, #0x0E ;
     STRB r7, [r3, #0x51C] ;write 1 to make pin digital
-
+    
+    LDRB r8, [r3, #0x3FC] ; GPIODATA
+    
 	CMP r0, #1
 	BEQ RED
 
-    ;how to control what colors show up ??
+ 	CMP r0, #2
+  	BEQ BLUE
 
-RED:
+   	CMP r0, #3
+	BEQ GREEN
+	
+	CMP r0, #4
+	BEQ PURPLE
+	
+	CMP r0, #5
+	BEQ YELLOW
+	
+	CMP r0, #6
+	BEQ WHITE
+    
+    
     ; RED , turn on pin 1, turn off pin 2 and 3
-    LDRB r8, [r3, #0x3FC] ; GPIODATA
-
+RED:
     ORR r9, r9, #0x02     ; need to turn pin 1 on that is 0000 0010
-
     STRB r9, [r3, #0x3FC]		; Turn on 0000 0010
 	B COLOR_STOP
 
-    ; BLUE
+    ; BLUE, turn on pin 2, turn off pin 1 and 3 so 0000 0100
+BLUE: 
+	ORR r9, r9, #0x04
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
 
-    ; GREEN
+    ; GREEN, turn on pin 3, turn off pin 1 and 2 so 0000 1000
+GREEN:
+	ORR r9, r9, #0x08
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
 
-    ; PURPLE
+    ; PURPLE = red + blue, pins 1 and 2 on, 3 off -> 0000 0110
+PURPLE: 
+	ORR r9, r9, #06
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
 
-    ; YELLOW
+    ; YELLOW = red + green, pins 1 and 3, 2 off -> 0000 1010
+YELLOW:
+	ORR r9, r9, #0x0A
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
 
-    ; WHITE
+    ; WHITE = red + blue + green, pins 1-3 on -> 0000 1110
+WHITE: 
+	ORR r9, r9, #0x0E
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
 
 COLOR_STOP:
 

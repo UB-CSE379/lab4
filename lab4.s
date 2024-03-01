709049cb0ec323f782f4bf536a3dce0b3e3adda1
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-03-01 18:33:24 -0500

    working on part 2 push btns

--- lab4.s
+++ lab4.s
@@ -1,10 +1,12 @@
 	.text
 	.global lab4
+	.global read_from_push_btns
 
 lab4:
 	PUSH {r4-r12,lr}	; Spill registers to stack
 
           ; Your code is placed here
+    BL read_from_push_btns
     BL read_tiva_pushbutton
     BL illuminate_RGB_LED
 
@@ -19,12 +21,12 @@ illuminate_RGB_LED:
           ; Your code is placed here
     ;Initialize Clock Address
     ; Color is passed in from r0
-    MOV r0, #1 	;RED 
+    ;MOV r0, #1 	;RED
     ;MOV r0, #2 ;BLUE
     ;MOV r0, #3 ;GREEN
     ;MOV r0, #4 ;PURPLE -> RED AND BLUE
     ;MOV r0, #5 ;YELLOW -> RED AND GREEN
-    ;MOV r0, #6 ;WHITE -> RED, BLUE, AND GREEN
+    MOV r0, #6 ;WHITE -> RED, BLUE, AND GREEN
 
     MOV r1, #0xE000
     MOVT r1, #0x400F
@@ -46,9 +48,9 @@ illuminate_RGB_LED:
 	; enable pin 1, 2, 3 as digital 0000 1110
     ORR r7, r7, #0x0E ;
     STRB r7, [r3, #0x51C] ;write 1 to make pin digital
-    
+
     LDRB r8, [r3, #0x3FC] ; GPIODATA
-    
+
 	CMP r0, #1
 	BEQ RED
 
@@ -57,17 +59,17 @@ illuminate_RGB_LED:
 
    	CMP r0, #3
 	BEQ GREEN
-	
+
 	CMP r0, #4
 	BEQ PURPLE
-	
+
 	CMP r0, #5
 	BEQ YELLOW
-	
+
 	CMP r0, #6
 	BEQ WHITE
-    
-    
+
+
     ; RED , turn on pin 1, turn off pin 2 and 3
 RED:
     ORR r9, r9, #0x02     ; need to turn pin 1 on that is 0000 0010
@@ -75,7 +77,7 @@ RED:
 	B COLOR_STOP
 
     ; BLUE, turn on pin 2, turn off pin 1 and 3 so 0000 0100
-BLUE: 
+BLUE:
 	ORR r9, r9, #0x04
 	STRB r9, [r3, #0x3FC]
 	B COLOR_STOP
@@ -87,7 +89,7 @@ GREEN:
 	B COLOR_STOP
 
     ; PURPLE = red + blue, pins 1 and 2 on, 3 off -> 0000 0110
-PURPLE: 
+PURPLE:
 	ORR r9, r9, #06
 	STRB r9, [r3, #0x3FC]
 	B COLOR_STOP
@@ -99,7 +101,7 @@ YELLOW:
 	B COLOR_STOP
 
     ; WHITE = red + blue + green, pins 1-3 on -> 0000 1110
-WHITE: 
+WHITE:
 	ORR r9, r9, #0x0E
 	STRB r9, [r3, #0x3FC]
 	B COLOR_STOP

6ba3ec90642308d084813d8f878e2df68eaadb5f
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-03-02 19:39:34 -0500

    gpio init done

--- library_lab_4.s
+++ library_lab_4.s
@@ -58,8 +58,9 @@ gpio_btn_and_LED_init:
 	STRB r9, [r7, #0x400]
 
 	;Port F Pin Direction is Input, Write 0 to pin 4
+	; Port F Pin Direction Output for pins 0 - 3, write 1
 	LDRB r9, [r8, #0x400]
-	AND r9, r9, #0x00
+	ORR r9, r9, #0x0E ; 0000 1110
 	STRB r9, [r8, #0x400]
 
     ;SET PIN AS DIGITAL
@@ -80,7 +81,7 @@ gpio_btn_and_LED_init:
 
     ; Set Pin 4 in Port F Digital, write 1
     LDRB r12, [r8, #0x51C]
-    ORR r12, r12, #0x10
+    ORR r12, r12, #0x1E
     STRB r12, [r8, #0x51C]
 
           ; Your code is placed here
@@ -226,18 +227,109 @@ LED_STOP:
 	POP {r4-r12,lr}  	; Restore registers from stack
 	MOV pc, lr
 
-;luminate_RGB_LED:
-;PUSH {r4-r12,lr}	; Spill registers to stack
+illuminate_RGB_LED:
 
-          ; Your code is placed here
-
-;POP {r4-r12,lr}  	; Restore registers from stack
-;MOV pc, lr
-
-read_tiva_pushbutton:
 	PUSH {r4-r12,lr}	; Spill registers to stack
 
           ; Your code is placed here
+    ;Initialize Clock Address
+    ; Color is passed in from r0
+    ;MOV r0, #1 	;RED
+    ;MOV r0, #2 ;BLUE
+    ;MOV r0, #3 ;GREEN
+    ;MOV r0, #4 ;PURPLE -> RED AND BLUE
+    MOV r0, #5 ;YELLOW -> RED AND GREEN
+    ;MOV r0, #6 ;WHITE -> RED, BLUE, AND GREEN
+
+    MOV r3, #0x5000
+    MOVT r3, #0x4002
+
+    LDRB r9, [r3, #0x3FC] ; GPIODATA
+
+	CMP r0, #1
+	BEQ RED
+
+ 	CMP r0, #2
+  	BEQ BLUE
+
+   	CMP r0, #3
+	BEQ GREEN
+
+	CMP r0, #4
+	BEQ PURPLE
+
+	CMP r0, #5
+	BEQ YELLOW
+
+	CMP r0, #6
+	BEQ WHITE
+
+
+    ; RED , turn on pin 1, turn off pin 2 and 3
+RED:
+    ORR r9, r9, #0x02     ; need to turn pin 1 on that is 0000 0010
+    STRB r9, [r3, #0x3FC]		; Turn on 0000 0010
+	B COLOR_STOP
+
+    ; BLUE, turn on pin 2, turn off pin 1 and 3 so 0000 0100
+BLUE:
+	ORR r9, r9, #0x04
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
+
+    ; GREEN, turn on pin 3, turn off pin 1 and 2 so 0000 1000
+GREEN:
+	ORR r9, r9, #0x08
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
+
+    ; PURPLE = red + blue, pins 1 and 2 on, 3 off -> 0000 0110
+PURPLE:
+	ORR r9, r9, #06
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
+
+    ; YELLOW = red + green, pins 1 and 3, 2 off -> 0000 1010
+YELLOW:
+	ORR r9, r9, #0x0A
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
+
+    ; WHITE = red + blue + green, pins 1-3 on -> 0000 1110
+WHITE:
+	ORR r9, r9, #0x0E
+	STRB r9, [r3, #0x3FC]
+	B COLOR_STOP
+
+COLOR_STOP:
+
+	POP {r4-r12,lr}  	; Restore registers from stack
+	MOV pc, lr
+
+read_tiva_push_button:
+	PUSH {r4-r12,lr}	; Spill registers to stack
+
+          ; Your code is placed here
+
+    ;Initialize r3 with Port F address
+    MOV r3, #0x5000
+    MOVT r3, #0x4002
+
+    LDRB r9, [r3, #0x3FC] ;GPIODATA
+    AND r9, r9, #0x10	;
+
+    CMP r9, #0x10 ; check if pin is being pressed
+    BNE PRESS ; if r9 == 0, r0 = 1
+    MOV r0, #0 ; if r9 == 1, r0 = 0
+    B STOP
+
+PRESS:
+	MOV r0, #1; button is being pressed
+
+
+STOP:
+
+    ;
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	MOV pc, lr

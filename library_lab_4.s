c87cdfa683a2010a918fb1100cb4d5fe78d2576b
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-03-02 19:02:27 -0500

    LED WORKS, Initialization gpio works

--- library_lab_4.s
+++ library_lab_4.s
@@ -24,8 +24,66 @@ uart_init:
 gpio_btn_and_LED_init:
 	PUSH {r4-r12,lr}	; Spill registers to stack
 
+	;Enable Clocks for Ports B, D, and F
+	MOV r4, #0xE000
+	MOVT r4, #0x400F
+	LDRB r5, [r4, #0x608]		  ;00  1  0  1  0  1  0
+	ORR r5, r5, #0x2A ;0010 1010  Port F, E, D, C, B, A
+	STRB r5, [r4, #0x608]
+
+	;Port F Pin 4 is input, write 0
+		; - need a pull-up resistor for SW1
+	; Port D Pins 0-3 are input for Btns, write 0
+	; Port B Pins 0-3 are output for LEDs, write 1
+	; Port B Base Address-> 0x40005000
+	MOV r6, #0x5000
+	MOVT r6, #0x4000
+	;Port D Base Address -> 0x40007000
+	MOV r7, #0x7000
+	MOVT r7, #0x4000
+
+	;Port F Base Address -> 0x40025000
+	MOV r8, #0x5000
+	MOVT r8, #0x4002
+
+	;Set Pin Directions
+	;Port B Pin Direction is Output, write 1 to pins 0 - 3
+	LDRB r9, [r6, #0x400]
+    ORR r9, r9, #0x0F ;configure pins 0 - 3 as output
+    STRB r9, [r6, #0x400] ; write 1 to mem
+
+	;Port D Pin Direction is Input, Write 0 to pins 0-3
+	LDRB r9, [r7, #0x400]
+	AND r9, r9, #0x00
+	STRB r9, [r7, #0x400]
+
+	;Port F Pin Direction is Input, Write 0 to pin 4
+	LDRB r9, [r8, #0x400]
+	AND r9, r9, #0x00
+	STRB r9, [r8, #0x400]
+
+    ;SET PIN AS DIGITAL
+    ; Set Pins 0-3 in Port B Digital, write 1
+    LDRB r10, [r6, #0x51C]
+    ORR r10, r10, #0x0F
+    STRB r10, [r6, #0x51C]
+
+    ; Set Pins 0-3 in Port D Digital, write 1
+    LDRB r11, [r7, #0x51C]
+    ORR r11, r11, #0x0F
+    STRB r11, [r7, #0x51C]
+
+    ; Initilize Pull-up resistor for Port F, write 1
+    LDRB r12, [r8, #0x510]
+    ORR r12, r12, #0x10
+    STRB r12, [r8, #0x510]
+
+    ; Set Pin 4 in Port F Digital, write 1
+    LDRB r12, [r8, #0x51C]
+    ORR r12, r12, #0x10
+    STRB r12, [r8, #0x51C]
+
           ; Your code is placed here
-    ;BL read_from_push_btns
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	MOV pc, lr
@@ -66,25 +124,9 @@ read_from_push_btns:
 	PUSH {r4-r12,lr}	; Spill registers to stack
 
           ; Your code is placed here
-    ;ENABLE CLOCK FOR PORT D
-    MOV r1, #0xE000  ; get the SYSCTL_RCGC address into r1
-    MOVT r1, #0x400F
-    LDRB r4, [r1, #0x608] ; load r4 with r1 mem address
-    ORR r4, r4, #0x08  ; write 1 to port D 0000 1000 to enable clock
-    STRB r4, [r1, #0x608] ;
-
-    ; CONFIGURE PIN AS INPUT
     ;Initilize r3 with PORT D Base Address
     MOV r3, #0x7000
     MOVT r3, #0x4000
-    LDRB r5, [r3, #0x400] ; Load r5 with GPIODIR address
- 	AND r5, r5, #0x00 ; Configure pin as Input
- 	STRB r5, [r3, #0x400] ; Write 0 to all pins 0000 0000 in r3
-
- 	;GPIODEN, make pins 0 - 3 digital
- 	LDRB r6, [r3, #0x51C]
- 	ORR r6, r6, #0x0F ; 0000 1111 make pins 0 - 3 digital
- 	STRB r6, [r3, #0x51C]
 
  	;GPIODATA
  	LDRB r9, [r3, #0x3FC]
@@ -130,17 +172,67 @@ illuminate_LEDs:
 	PUSH {r4-r12,lr}	; Spill registers to stack
 
           ; Your code is placed here
+    ;Get LEDS
+    ;MOV r0, #0 ;First LED
+    ;MOV r0, #1 ; Second LED
+    ;MOV r0, #2 ; Third LED
+    ;MOV r0, #3; 4th LED
+    MOV r0, #5 ; ALL LEDS
+
+	; Get Port B Base Address
+	MOV r6, #0x5000
+	MOVT r6, #0x4000
+
+	LDRB r1, [r6, #0x3FC]
+    CMP r0, #0
+    BEQ LED0
+
+    CMP r0, #1
+    BEQ LED1
+
+    CMP r0, #2
+    BEQ LED2
+
+    CMP r0, #3
+    BEQ LED3
+
+    CMP r0, #5
+    BEQ LEDALL
+LED0:
+	ORR r1, r1, #0x01
+	STRB r1, [r6, #0x3FC]
+	B LED_STOP
+
+LED1:
+	ORR r1, r1, #0x02
+	STRB r1, [r6, #0x3FC]
+	B LED_STOP
+
+LED2:
+	ORR r1, r1, #0x04
+	STRB r1, [r6, #0x3FC]
+	B LED_STOP
+
+LED3:
+	ORR r1, r1, #0x08
+	STRB r1, [r6, #0x3FC]
+	B LED_STOP
+LEDALL:
+	ORR r1, r1, #0x0F
+	STRB r1, [r6, #0x3FC]
+	B LED_STOP
+LED_STOP:
 
 	POP {r4-r12,lr}  	; Restore registers from stack
 	MOV pc, lr
 
-illuminate_RGB_LED:
-	PUSH {r4-r12,lr}	; Spill registers to stack
+;luminate_RGB_LED:
+;PUSH {r4-r12,lr}	; Spill registers to stack
 
           ; Your code is placed here
 
-	POP {r4-r12,lr}  	; Restore registers from stack
-	MOV pc, lr
+;POP {r4-r12,lr}  	; Restore registers from stack
+;MOV pc, lr
 
 read_tiva_pushbutton:
 	PUSH {r4-r12,lr}	; Spill registers to stack
@@ -159,4 +251,3 @@ div_and_mod:
 	MOV pc, lr
 
 	.end
-

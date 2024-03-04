44ee49fde47e2d741698a69d41c4b0348ddd42ed
Muhtasim Mushfiq <90406426+muhtasim24@users.noreply.github.com>
2024-03-04 09:39:54 -0500

    updated globals

--- lab4.s
+++ lab4.s
@@ -1,6 +1,16 @@
 	.text
 	.global lab4
+	.global uart_init
+	.global gpio_btn_and_LED_init
+	.global output_character
+	.global read_character
+	.global read_string
+	.global output_string
 	.global read_from_push_btns
+	.global illuminate_LEDs
+	.global illuminate_RGB_LED
+	.global read_tiva_push_button
+	.global div_and_mod
 
 lab4:
 	PUSH {r4-r12,lr}	; Spill registers to stack
